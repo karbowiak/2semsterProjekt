8dfa95fc031170b58bd1ba33ee52e8a0cbcf303b
dmaa0215_33 <dmaa0215_33@83b57740-2875-1f43-8e9e-e5a215aa8d68>
2015-12-05 21:52:11 +0000

    Merge branch 'master' into svn
    git-svn-id: https://kraka.ucn.dk/svn/dmaa0215_2Sem_6/trunk@3 83b57740-2875-1f43-8e9e-e5a215aa8d68

---
+++ src/main/java/config/mhcConfigNew.java
@@ -0,0 +1,24 @@
+package config;
+
+public class mhcConfigNew {
+    public static final String driver = "";
+    public static final String databaseName = "";
+    public static final String username = "";
+    public static final String password = "";
+
+    public static String getDriver() {
+        return driver;
+    }
+
+    public static String getDatabaseName() {
+        return databaseName;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
