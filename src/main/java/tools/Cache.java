f759a13c43f51a6455a4a9db92bca5929c10755c
Michael Karbowiak <karbowiak@gmail.com>
2015-12-13 03:16:23 +0100

    add the Console controller package
    and add some text to Cache

--- src/main/java/tools/Cache.java
+++ src/main/java/tools/Cache.java
@@ -2,7 +2,12 @@ package tools;
 
 public class Cache {
     public Cache() {
+        // should startup a hashtable here with some sane size
+    }
 
+    public static void cleanup() {
+        // everytime a get is done, call cleanup - ugly, sure, but could also put it on a timer in a seperate thread
+        // not sure how to do that yet tho..
     }
 
     public static boolean get(String key) {
@@ -12,6 +17,4 @@ public class Cache {
     public static boolean set(String key, String value) {
         return false;
     }
-
-
 }
