87841ccc93f717846c0b8747ae3bab488cf29ce6
Michael Karbowiak <karbowiak@gmail.com>
2016-01-11 10:01:34 +0100

    Add Time model, it's empty atm

---
+++ src/main/java/modelLayer/Time.java
@@ -0,0 +1,16 @@
+package modelLayer;
+
+import tools.QuickHash;
+import tools.databaseQuery;
+
+import java.sql.SQLException;
+import java.util.*;
+
+import static tools.databaseQuery.dbQuery;
+import static tools.databaseQuery.dbQueryRow;
+import static tools.databaseQuery.dbQueryField;
+import static tools.databaseQuery.dbExecute;
+
+public class Time {
+    databaseQuery db = new databaseQuery();
+}
